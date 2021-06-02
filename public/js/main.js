1d734834fc955a35c9e8138eb98041cc53f5dcf1
Ashtar Paniagua <ashtarcodes@gmail.com>
2021-06-02 08:21:09 -0400

    heroku troubleshoot

--- public/js/main.js
+++
@@ -1,4 +0,0 @@
-import Popper from '.../node_modules/@popperjs/core/dist/umd/popper-base.min.js'
-import Bootstrap from '.../node_modules/bootstrap/dist/js/bootstrap.min.js'
-
-export { Popper, Bootstrap}
