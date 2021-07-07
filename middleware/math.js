19d42ad45afa9423e729e35e14db5b418be57f61
Ashtar Paniagua <74083145+AshtarCodes@users.noreply.github.com>
2021-07-06 21:39:27 -0400

    sumIngredientQuantities handles conversion of small/large quantities, and definedUnits refactored to contain options.

--- middleware/math.js
+++ middleware/math.js
@@ -89,14 +89,14 @@ math.createUnit('clove', { definition: '1 teaspoon', aliases: ['cloves'] });
 
 // consider turning this into class methods that manage amounts returned
 const definedUnits = {
-  teaspoon: 'tablespoon',
-  tablespoon: 'cup',
-  cup: 'cup',
-  stalk: 'stalk',
-  pound: 'pound',
-  bunch: 'bunch',
-  clove: 'cloves',
-  ounce: 'ounce',
+  teaspoon: { standard: 'teaspoon', small: 'teaspoon', large: 'tablespoon' },
+  tablespoon: { standard: 'tablespoon', small: 'teaspoon', large: 'cup' },
+  cup: { standard: 'cup', small: 'tablespoon', large: null },
+  stalk: { standard: 'stalk', small: null, large: null },
+  pound: { standard: 'pound', small: null, large: null },
+  bunch: { standard: 'bunch', small: null, large: null },
+  clove: { standard: 'cloves', small: null, large: null },
+  ounce: { standard: 'ounce', small: null, large: 'pound' },
 };
 
 const ignoreProductUnits = [
@@ -109,6 +109,7 @@ const ignoreProductUnits = [
   'paprika',
   'powder',
   'sauce',
+  'cayenne pepper',
 ];
 
 // P: obj of arrays; R: Obj of string; E: {product: '5 items'};
@@ -122,48 +123,36 @@ function sumIngredientQuantities(obj) {
     // eslint-disable-next-line no-restricted-syntax
     // eslint-disable-next-line guard-for-in
     for (const item in itemQuantities) {
-      // loop through sub arrays
       if (!ignoreProductUnits.some((el) => item.includes(el))) {
+        // If product not in the "ignore" list, then
         const quantities = itemQuantities[item];
-        // console.log(`QUANTITIES: `, quantities);
 
         // parse array of quantity arrays
         for (const quantity of quantities) {
           const num = quantity[0];
+          // if it's a whole item (e.g 1 banana), unit is null.
           let unit = quantity[1] ? quantity[1] : null;
-          // console.log(`NUM AND UNIT: `, num, unit);
 
-          // if it's a whole item, unit is null.
           if (unit == null) {
             finalQuantity.push(num);
           } else if (!definedUnits[unit]) {
-            // for now, ignore it. eventually define it and convert.
+            // TODO: If unit is not recognized, then define it
+            // right now, it leaves finalQuantity empty.
           } else if (definedUnits[unit]) {
-            // if it's a nice unit, do some conversion and add things
-            // ~!!~ TODO: implement ignore list, and handle small item quantities ~~!!~~***
-
-            const unitToConvertTo = definedUnits[unit];
-            unit = math.unit(num, unit).to(unitToConvertTo);
-
-            /* If quantity is small, convert current unit to a smaller unit. E.g: 0.1 cup to tablespoons. 
-            if (unit.toNumber() < 1) {
-            }
-            */
+            const unitToConvertTo = definedUnits[unit].standard; // teaspoon returned as tablespoon unit
+            unit = math.unit(num, unit).to(unitToConvertTo); // 3 teaspoon -> 1 tablespoon
 
             finalQuantity.push(unit);
           }
         }
-
+        // ! End of for-of quantities loop
         // if product quantities are all whole items (e.g 1 red pepper), add them up
         if (finalQuantity.every((el) => typeof el === 'number')) {
-          finalQuantity = `${finalQuantity.reduce(
-            (acc, c) => acc + c,
-            0
-          )} item`;
+          const amount = finalQuantity.reduce((acc, c) => acc + c, 0);
+          finalQuantity = `${amount} ${amount > 1 ? 'items' : 'item'}`;
 
-          // if product quantities are defined units in our system, add them up
+          // if product quantities are defined mathjs units in our system, add them up
         } else if (finalQuantity.every((el) => math.typeOf(el) === 'Unit')) {
-          // if all elements are mathjs units, add them up
           finalQuantity = finalQuantity.reduce((acc, unit, i) => {
             if (i === 0) {
               const start = math.unit(0, unit.formatUnits());
@@ -173,11 +162,45 @@ function sumIngredientQuantities(obj) {
             }
             return acc;
           }, 0);
-          // display with 1 decimal place and unit: 1.1 cup
-          finalQuantity = `${finalQuantity
-            .toNumber()
-            .toFixed(1)} ${finalQuantity.formatUnits()}`;
 
+          const num = finalQuantity.toNumber();
+
+          const unit = finalQuantity.formatUnits();
+
+          // display with 1 decimal place and unit: 1.1 cup
+          let displayQuantity = `${num.toFixed(1)} ${unit}`;
+
+          // TODO: Handle small quantities. 1 tsp vinegar -> 0.3 tbsp. 1 pinch -> 0 item
+          /* ! If quantity is small, convert current unit to a smaller unit. E.g: 0.1 cup to tablespoons.
+           */
+          // '< 1/2 tablespoon => teaspoon', '< '
+          let unitToConvertTo;
+          let convertedUnit; // teaspoon returned as tablespoon unit
+          if (
+            (num < 0.5 && unit === 'tablespoon') ||
+            (num < 0.1 && unit === 'cup')
+          ) {
+            unitToConvertTo = definedUnits[unit].small;
+            console.log('CONVERT TO SMALL: ', unitToConvertTo);
+          } else if (
+            (num > 4 && unit === 'tablespoon') ||
+            (num >= 3 && unit === 'teaspoon')
+          ) {
+            unitToConvertTo = definedUnits[unit].large;
+            console.log('CONVERT TO LARGE : ', unitToConvertTo);
+          }
+          // 3 teaspoon -> 1 tablespoon
+          convertedUnit =
+            unitToConvertTo != undefined
+              ? math.unit(num, unit).to(unitToConvertTo)
+              : null;
+
+          displayQuantity =
+            convertedUnit !== null ? convertedUnit : displayQuantity;
+
+          finalQuantity = displayQuantity;
+
+          console.log('ITEM QUANTITY: ', finalQuantity);
           // if not a whole item or a defined unit, then leave it blank
         } else {
           finalQuantity = null;
@@ -186,10 +209,11 @@ function sumIngredientQuantities(obj) {
         // if in the ignored product units list, provide no quantity
         finalQuantity = null;
       }
-      // set the current product to this single quantity
-      itemQuantities[item] = finalQuantity || null;
+      /* set the current product to this single quantity
+      E.g. { garlic: 10 cloves }
+      */
+      itemQuantities[item] = finalQuantity !== '0 item' ? finalQuantity : null;
 
-      // reset the quantity
       finalQuantity = [];
     }
     return itemQuantities;
